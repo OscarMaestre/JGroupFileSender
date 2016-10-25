f1a1c5b6b857e2c030b8cc537c1790c6e6cf9463
oscar <oscar@example.com>
2016-10-25 22:57:40 +0200

    Associated IP address

--- src/com/gomezoscar/jgroupfilesender/sender/FileSender.java
+++ src/com/gomezoscar/jgroupfilesender/sender/FileSender.java
@@ -4,6 +4,7 @@ import java.net.MulticastSocket;
 
 public class FileSender {
 	private MulticastSocket senderSocket=null;
+	private InetAddress groupIP;
 	public FileSender ( String groupIP ){
 		
 	}
