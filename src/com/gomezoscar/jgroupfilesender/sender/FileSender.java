016382329c105f0fd41571b4dbae76acaa1479fc
Oscar GG <profesor.oscar.gomez@gmail.com>
2016-10-25 17:36:00 +0200

    Inicial

---
+++ src/com/gomezoscar/jgroupfilesender/sender/FileSender.java
@@ -0,0 +1,10 @@
+package com.gomezoscar.jgroupfilesender.sender;
+
+import java.net.MulticastSocket;
+
+public class FileSender {
+	private MulticastSocket senderSocket=null;
+	public FileSender ( String groupIP ){
+		
+	}
+}
